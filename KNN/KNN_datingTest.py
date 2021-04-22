78ce6bf6ade21949533b98c1f0dd713c57ee6b1b
fence <imfence99@gmail.com>
2021-04-22 18:03:23 +0800

    update

--- KNN/KNN_datingTest.py
+++ KNN/KNN_datingTest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import KNN_0
 
 
 def file2matrix(filename):
@@ -24,9 +25,8 @@ def file2matrix(filename):
     return returnMat, classLabelVec
 
 
-datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+#datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
 # print(datingDataMat,'\n',datingLabels)
-
 """ fig,ax = plt.subplots(1,1)
 ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*np.array(datingLabels),15.0*np.array(datingLabels))
 plt.show() """
@@ -42,9 +42,29 @@ def autoNorm(dataSet):
     normDataSet = (dataSet - np.tile(minVals, (m, 1))) / ranges
     return normDataSet, ranges, minVals
 
-normMat, ranges, minVals=autoNorm(datingDataMat)
+#normMat, ranges, minVals=autoNorm(datingDataMat)
 #print(normMat, '\n',ranges, '\n',minVals)
+
 
 def datingClassTest():
     hoRatio = 0.10
-    
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        clfResult = KNN_0.classify0(
+            normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 4)
+        print("The classifier came back with: %d\n, the real answer is: %d" %
+              (clfResult, datingLabels[i]))
+        if(clfResult != datingLabels[i]):
+            errorCount += 1.0
+    print("The total error rate is:%f" % (errorCount/float(numTestVecs)))
+
+
+# datingClassTest()
+
+def clfPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats=
