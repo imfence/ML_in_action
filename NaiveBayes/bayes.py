3429ecbf2b7942d10040a1562f7d69cebc410049
fence <imfence99@gmail.com>
2021-05-16 20:37:59 +0800

    i find what the book says is wrong, so i dont want to continue this chapter

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -1,35 +1,44 @@
-from numpy import *
+import numpy as np
+from functools import reduce
+
 
 def loadDataSet():
-    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
-                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
-                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
-                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0,1,0,1,0,1]
-    return postingList,classVec
-                 
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak',
+                       'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+
 def createVocabList(dataSet):
-    vocabSet = set([]) 
+    vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
+
 
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
-        else: print("the word: %s is not in my Vocabulary!" % word)
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
 
-""" def trainNB0(trainMatrix,trainCategory):
+
+def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = ones(numWords); p1Num = ones(numWords)      #change to ones() 
-    p0Denom = 2.0; p1Denom = 2.0                        #change to 2.0
+    p0Num = np.zeros(numWords)
+    p1Num = np.zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -37,38 +46,44 @@ def setOfWords2Vec(vocabList, inputSet):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = log(p1Num/p1Denom)          #change to log()
-    p0Vect = log(p0Num/p0Denom)          #change to log()
-    return p0Vect,p1Vect,pAbusive
+    # print(p1Denom)
+    # print(p0Denom)
+    p1Vect = p1Num/p1Denom
+    p0Vect = p0Num/p0Denom
+    return p0Vect, p1Vect, pAbusive
+
 
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-    p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
-    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    p1 = reduce(lambda x, y: x*y, vec2Classify * p1Vec) * (pClass1)
+    p0 = reduce(lambda x, y: x*y, vec2Classify * p0Vec) * (1.0 - pClass1)
     if p1 > p0:
         return 1
-    else: 
+    else:
         return 0
-    
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
+
+"""   
 def bagOfWords2VecMN(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] += 1
     return returnVec
-
-def testingNB():
-    listOPosts,listClasses = loadDataSet()
-    myVocabList = createVocabList(listOPosts)
-    trainMat=[]
-    for postinDoc in listOPosts:
-        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
-    p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
-    testEntry = ['love', 'my', 'dalmation']
-    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
-    testEntry = ['stupid', 'garbage']
-    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
 
 def textParse(bigString):    #input is big string, #output is word list
     import re
@@ -164,14 +179,20 @@ def getTopWords(ny,sf):
     print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
     for item in sortedNY:
         print item[0]
- """
+"""
 
 if __name__ == '__main__':
-    postingList, classVec = loadDataSet()
-    print('postingList:\n',postingList)
+    """ postingList, classVec = loadDataSet()
+    # print('postingList:\n',postingList)
     myVocabList = createVocabList(postingList)
     print('myVocabList:\n', myVocabList)
     trainMat = []
     for postinDoc in postingList:
-        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
-    print('trainMat:\n',trainMat)
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    # print('trainMat:\n',trainMat)
+    p0V, p1V, pAb = trainNB0(trainMat, classVec)
+    print('p0V:\n', p0V)
+    print('p1V:\n', p1V)
+    print('classVec:\n', classVec)
+    print('pAb:\n', pAb) """
+    testingNB()
