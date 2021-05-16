2a05e74af072ece1282f638ed91be0dcfc77673e
fence <imfence99@gmail.com>
2021-05-16 22:17:52 +0800

    update

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -35,10 +35,10 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = np.zeros(numWords)
-    p1Num = np.zeros(numWords)
-    p0Denom = 0.0
-    p1Denom = 0.0
+    p0Num = np.ones(numWords)
+    p1Num = np.ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -56,8 +56,8 @@ def trainNB0(trainMatrix, trainCategory):
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
     p0 = sum(vec2Classify * p0Vec) * np.log(1.0 - pClass1)
-    #print(p1)
-    #print(p0)
+    # print(p1)
+    # print(p0)
     if p1 > p0:
         return 1
     else:
@@ -77,7 +77,7 @@ def testingNB():
     testEntry = ['stupid', 'garbage']
     thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
     print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
- 
+
 
 def bagOfWords2VecMN(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
@@ -86,42 +86,50 @@ def bagOfWords2VecMN(vocabList, inputSet):
             returnVec[vocabList.index(word)] += 1
     return returnVec
 
+
 def textParse(bigString):
     import re
-    listOfTokens = re.split(r'\W*', bigString)
-    return [tok.lower() for tok in listOfTokens if len(tok) > 2] 
-    
+    listOfTokens = re.split(r'[\W*]', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
 def spamTest():
-    docList=[]; classList = []; fullText =[]
-    for i in range(1,26):
-        wordList = textParse(open('email/spam/%d.txt' % i).read())
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('./email/spam/%d.txt' % i,'r').read())
+        #print(wordList)
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(1)
-        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        wordList = textParse(open('./email/ham/%d.txt' % i,'r').read())
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
     vocabList = createVocabList(docList)
-    trainingSet = list(range(50)); testSet=[]
+    #print(vocabList)
+    trainingSet = list(range(50))
+    testSet = []
     for i in range(10):
-        randIndex = int(np.random.uniform(0,len(trainingSet)))
+        randIndex = int(np.random.uniform(0, len(trainingSet)))
         print(randIndex)
         testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])  
-    trainMat=[]; trainClasses = []
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
     for docIndex in trainingSet:
         trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
         trainClasses.append(classList[docIndex])
-    p0V,p1V,pSpam = trainNB0(np.array(trainMat),np.array(trainClasses))
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))
     errorCount = 0
     for docIndex in testSet:
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
-        if classifyNB(np.array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
             errorCount += 1
-            print("classification error",docList[docIndex])
-    print('the error rate is: ',float(errorCount)/len(testSet))
-    
+            print("classification error", docList[docIndex])
+    print('the error rate is: %f' % (float(errorCount)/len(testSet)))
+
 """ def calcMostFreq(vocabList,fullText):
     import operator
     freqDict = {}
@@ -196,5 +204,5 @@ if __name__ == '__main__':
     print('p1V:\n', p1V)
     print('classVec:\n', classVec)
     print('pAb:\n', pAb) """
-    #testingNB()
+    # testingNB()
     spamTest()
