9db778aba031acb81caebe955734fa9fa0643bd1
fence <imfence99@gmail.com>
2021-05-16 17:55:43 +0800

    update

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -1,8 +1,3 @@
-'''
-Created on Oct 19, 2010
-
-@author: Peter
-'''
 from numpy import *
 
 def loadDataSet():
@@ -12,24 +7,24 @@ def loadDataSet():
                  ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
                  ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    classVec = [0,1,0,1,0,1]
     return postingList,classVec
                  
 def createVocabList(dataSet):
-    vocabSet = set([])  #create empty set
+    vocabSet = set([]) 
     for document in dataSet:
-        vocabSet = vocabSet | set(document) #union of the two sets
+        vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
 def setOfWords2Vec(vocabList, inputSet):
-    returnVec = [0]*len(vocabList)
+    returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
-        else: print "the word: %s is not in my Vocabulary!" % word
+        else: print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
 
-def trainNB0(trainMatrix,trainCategory):
+""" def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
@@ -169,3 +164,14 @@ def getTopWords(ny,sf):
     print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
     for item in sortedNY:
         print item[0]
+ """
+
+if __name__ == '__main__':
+    postingList, classVec = loadDataSet()
+    print('postingList:\n',postingList)
+    myVocabList = createVocabList(postingList)
+    print('myVocabList:\n', myVocabList)
+    trainMat = []
+    for postinDoc in postingList:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    print('trainMat:\n',trainMat)
