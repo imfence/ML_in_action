d0e6764b08d2420a7c4310c5028d691654e72e94
fence <imfence99@gmail.com>
2021-05-17 10:45:50 +0800

    update

--- LogisticRegression/logRegres.py
+++ LogisticRegression/logRegres.py
@@ -14,16 +14,16 @@ def sigmoid(inX):
     return 1.0/(1+np.exp(-inX))
 
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = np.mat(dataMatIn)             #convert to NumPy matrix
-    labelMat = np.mat(classLabels).transpose() #convert to NumPy matrix
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()
     m,n = np.shape(dataMatrix)
     alpha = 0.001
     maxCycles = 500
     weights = np.ones((n,1))
-    for k in range(maxCycles):              #heavy on matrix operations
-        h = sigmoid(dataMatrix*weights)     #matrix mult
-        error = (labelMat - h)              #vector subtraction
-        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose()* error
     return weights
 
 def plotBestFit(weights):  
@@ -50,7 +50,7 @@ def plotBestFit(weights):
 def stocGradAscent0(dataMatrix, classLabels):
     m,n = np.shape(dataMatrix)
     alpha = 0.01
-    weights = np.ones(n)   #initialize to all ones
+    weights = np.ones(n)
     for i in range(m):
         h = sigmoid(sum(dataMatrix[i]*weights))
         error = classLabels[i] - h
@@ -59,12 +59,12 @@ def stocGradAscent0(dataMatrix, classLabels):
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = np.shape(dataMatrix)
-    weights = np.ones(n)   #initialize to all ones
+    weights = np.ones(n)
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
-            randIndex = int(np.random.uniform(0,len(dataIndex)))#go to 0 because of the constant
+            alpha = 4/(1.0+j+i)+0.0001
+            randIndex = int(np.random.uniform(0,len(dataIndex)))
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
